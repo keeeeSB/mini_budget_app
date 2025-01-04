cd32319459e6f8a5597d500ab2d2bf0302dfed1d
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-01-04 15:20:07 +0900

    Userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: URI::MailTo::EMAIL_REGEXP }
+                    uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
