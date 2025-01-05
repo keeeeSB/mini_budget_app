e9ac20dae9d9f36c94c331114c3f7e7c26456492
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-01-05 21:39:09 +0900

    confirmableを使用し、アカウント承認を有効化

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_04_054614) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_05_123647) do
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
@@ -20,6 +20,10 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_04_054614) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
