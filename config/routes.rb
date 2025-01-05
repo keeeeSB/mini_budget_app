7275f3b4471df45f6a32a18cffc59cade5d39562
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-01-06 08:28:38 +0900

    アカウント承認メールの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'homes/index'
 
-  devise_for :users
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  
+  devise_for :users, controllers: {
+    registrations: "users/registrations"
+  }
 
   authenticated :user do
   end
