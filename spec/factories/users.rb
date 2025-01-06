732029585f5cffc768d2aacfe355dfc82e7c1841
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-01-06 12:50:45 +0900

    ログイン機能のテストを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
     sequence(:email) { |n| "tester#{n}@example.com" }
     password { "password" }
     password_confirmation { "password" }
+    confirmed_at { Time.current }
   end
 end
