2f326ef38be0b2c1a316c75cd0199dda399c2cda
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-01-07 12:28:36 +0900

    バリデーションの追加

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,7 @@
 class Entry < ApplicationRecord
   belongs_to :user
+
+  validates :date, presence: true
+  validates :amount, presence: true
+  validates :description, length: { maximum: 100 }
 end
