80bdc3187c1c9e087d88d91d58e21eb94fc1e64c
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-01-08 12:22:02 +0900

    Category, Entryモデルを中間テーブルを使用しリレーションを追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
+  has_many :category_entries, dependent: :destroy
+  has_many :categories, through: :category_entries
+
   validates :name, presence: true, uniqueness: true
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,7 @@
 class Entry < ApplicationRecord
   belongs_to :user
+  has_many :category_entries, dependent: :destroy
+  has_many :categories, through: :category_entries
 
   validates :date, presence: true
   validates :amount, presence: true
