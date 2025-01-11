4bce554ff416487cc43b60068af17c9ce742edb2
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-01-11 13:12:05 +0900

    FactoryBotの修正

--- spec/factories/entries.rb
+++ spec/factories/entries.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :entry do
-    date { "2025-01-07" }
+    date { "Wed, 01 Jan 2025" }
     amount { "1000" }
     description { "今月はエアコンを多く使った。" }
     association :user
